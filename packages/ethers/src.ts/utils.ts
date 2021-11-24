042244a6d1681706970de67f39a8938fb42e81d2
Daniel K Ivanov <daniel.k.ivanov95@gmail.com>
2021-11-24 15:51:04 +0200

    add(address-utils)
    Signed-off-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>

--- packages/ethers/src.ts/utils.ts
+++ packages/ethers/src.ts/utils.ts
@@ -1,7 +1,7 @@
 "use strict";
 
 import { AbiCoder, checkResultErrors, ConstructorFragment, defaultAbiCoder, ErrorFragment, EventFragment, FormatTypes, Fragment, FunctionFragment, Indexed, Interface, LogDescription, ParamType, Result, TransactionDescription }from "@ethersproject/abi";
-import { getAddress, getCreate2Address, getContractAddress, getIcapAddress, isAddress } from "@ethersproject/address";
+import { getAddress, getCreate2Address, getContractAddress, getIcapAddress, isAddress, getAccountFromAddress, getAddressFromAccount, parseAccount, Account, AccountLike } from "@ethersproject/address";
 import * as base64 from "@ethersproject/base64";
 import { Base58 as base58 } from "@ethersproject/basex";
 import { arrayify, concat, hexConcat, hexDataSlice, hexDataLength, hexlify, hexStripZeros, hexValue, hexZeroPad, isBytes, isBytesLike, isHexString, joinSignature, zeroPad, splitSignature, stripZeros } from "@ethersproject/bytes";
@@ -210,6 +210,17 @@ export {
     FetchJsonResponse,
 
     EncryptOptions,
-    ProgressCallback
+    ProgressCallback,
+
+    ////////////////////////
+    // Hedera
+    getAddressFromAccount,
+    getAccountFromAddress,
+    parseAccount,
+
+    ////////////////////////
+    // Hedera Types
+    Account,
+    AccountLike
 }
 
