a7e19c6063996e36e0a36f86dd7ef2982f6ba6f1
Daniel Ivanov <daniel.k.ivanov95@gmail.com>
2022-01-13 17:36:18 +0200

    refactor: simplify example
    Signed-off-by: Daniel Ivanov <daniel.k.ivanov95@gmail.com>

--- examples/provider.example.ts
+++ examples/provider.example.ts
@@ -26,7 +26,13 @@ const hethers = require("ethers");
     console.log(record);
 
     /**
-     * Example 3: Connecting to custom consensus and mirror node URLs via HederaProvider
+     * Example 3: Submitting Signed Transaction
+     */
+    const signedTx = "0x0aba012ab7010a4d0a1a0a0c08b2a1818e0610ca969fb10212080800100018d7ce20180012060800100018031880c2d72f220208783200721c0a1a0a0a0a0608001000186210020a0c0a080800100018d7ce20100112660a640a200ba029dd87a99c3089e0b1a062fb6c97300a1856638009345d2a40bd6cb169db1a40766dede346621caa3c19b504fbaf414a4cccff7f989915e4367621ffe04ccf8435576044ff90f2117078fcec26c82e08733c49e6099e0bc2b996ca0edeb79d06";
+    await provider.sendTransaction(signedTx);
+
+    /**
+     * Example 4: Connecting to custom consensus and mirror node URLs via HederaProvider
      */
     const consensusNodeUrl = '0.testnet.hedera.com:50211';
     const consensusNodeId = '0.0.3';
@@ -38,5 +44,7 @@ const hethers = require("ethers");
 
     const record2 = await provider2.getTransaction(txId);
     console.log(record2);
+
+
 })();
 
