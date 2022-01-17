b57311463f326b20a866f335ec4288dd19fd9d5b
alshapi <alexander.shapkarov@limechain.tech>
2022-01-17 11:58:55 +0200

    update examples account to previewnet

--- examples/signed-contract-call.ts
+++ examples/signed-contract-call.ts
@@ -2,13 +2,16 @@ import {ethers} from 'ethers';
 import {AccountId, ContractExecuteTransaction, TransactionId, PrivateKey} from '@hashgraph/sdk';
 
 (async () => {
-    const operatorId = AccountId.fromString("0.0.1546615");
-    const privateKey = PrivateKey.fromString("302e020100300506032b657004220420c7cb0caa71b05f62dafaf426052b317dad25aa66228d6faa3291712efd86c6b8");
-	const provider = ethers.providers.getDefaultProvider("testnet");
+    // const operatorId = AccountId.fromString("0.0.1546615");
+    // const privateKey = PrivateKey.fromString("302e020100300506032b657004220420c7cb0caa71b05f62dafaf426052b317dad25aa66228d6faa3291712efd86c6b8");
+	// const provider = ethers.providers.getDefaultProvider("testnet");
+	const operatorId = AccountId.fromString("0.0.1385");
+    const privateKey = PrivateKey.fromString("302e020100300506032b6570042204209310582ff3b50e295ce3322ef98d92a4cc25bad5a857db7eb3481eaa3e77b249");
+	const provider = ethers.providers.getDefaultProvider("previewnet");
 
     const tx = await new ContractExecuteTransaction()
-		.setContractId("0.0.25623322")
-		.setFunction("mint")
+		.setContractId("0.0.28384291")
+		// .setFunction("mint")
 		.setGas(300000)
 		.setTransactionMemo("ContractExecuteTransaction memo")
 		.setTransactionId(TransactionId.generate(operatorId))
--- examples/signed-contract-create.ts
+++ examples/signed-contract-create.ts
@@ -3,15 +3,15 @@ import {AccountId, ContractCreateTransaction, ContractFunctionParameters, Transa
 
 // main
 (async () => {
-    const operatorId = AccountId.fromString("0.0.1546615");
-    const privateKey = PrivateKey.fromString("302e020100300506032b657004220420c7cb0caa71b05f62dafaf426052b317dad25aa66228d6faa3291712efd86c6b8");
-	const provider = ethers.providers.getDefaultProvider("testnet");
+    const operatorId = AccountId.fromString("0.0.1385");
+    const privateKey = PrivateKey.fromString("302e020100300506032b6570042204209310582ff3b50e295ce3322ef98d92a4cc25bad5a857db7eb3481eaa3e77b249");
+	const provider = ethers.providers.getDefaultProvider("previewnet");
 
     const tx = await new ContractCreateTransaction()
         .setContractMemo("ContractCreateTransaction memo")
         .setGas(300000)
         .setInitialBalance(1000)
-        .setBytecodeFileId("0.0.26568171")
+        .setBytecodeFileId("0.0.29970")
         .setNodeAccountIds([new AccountId(0,0,3)])
         // .setConstructorParameters(new ContractFunctionParameters().addUint256(100))
         .setTransactionId(TransactionId.generate(operatorId))
