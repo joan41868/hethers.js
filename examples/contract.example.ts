f7833b0f2cbaa979a483c37e2c48a323fcb9865f
alshapi <alexander.shapkarov@limechain.tech>
2022-01-18 16:01:00 +0200

    update contract.example, wait for receipt

--- examples/contract.example.ts
+++ examples/contract.example.ts
@@ -12,15 +12,15 @@ import { Key } from "@hashgraph/proto";
 
 const account = {
 	"operator": {
-		"accountId": "0.0.19041642",
-		"publicKey": "302a300506032b6570032100049d07fb89aa8f5e54eccd7b92846d9839404e8c0af8489a9a511422be958b2f",
-		"privateKey": "302e020100300506032b6570042204207ef3437273a5146e4e504a6e22c5caedf07cb0821f01bc05d18e8e716f77f66c"
+		"accountId": "0.0.1391",
+		"publicKey": "302a300506032b6570032100659152a5ced818eb7cbfe0e778f5b967c892963c0be798039857b8f5ace324dd",
+		"privateKey": "302e020100300506032b6570042204209310582ff3b50e295ce3322ef98d92a4cc25bad5a857db7eb3481eaa3e77b249"
 	},
 	"network": {
-		"0.testnet.hedera.com:50211": "0.0.3",
-		"1.testnet.hedera.com:50211": "0.0.4",
-		"2.testnet.hedera.com:50211": "0.0.5",
-		"3.testnet.hedera.com:50211": "0.0.6"
+		"35.231.208.148:50211": "0.0.3",
+		"35.199.15.177:50211": "0.0.4",
+		"35.225.201.195:50211": "0.0.5",
+		"35.247.109.135:50211": "0.0.6"
 	}
 };
 
@@ -29,7 +29,7 @@ const account = {
 	const edPrivateKey = PrivateKey.fromString(account.operator.privateKey);
 	const client = Client.forNetwork(account.network);
 	const generatedWallet = hethers.Wallet.createRandom();
-	const provider = hethers.providers.getDefaultProvider('testnet');
+	const provider = hethers.providers.getDefaultProvider('previewnet');
 	const protoKey = Key.create({
 		ECDSASecp256k1: arrayify(generatedWallet._signingKey().compressedPublicKey)
 	});
@@ -58,18 +58,36 @@ const account = {
 		data: contractByteCode,
 		gasLimit: 300000
 	});
-	console.log(contractCreateResponse);
+	console.log("contractCreateResponse", contractCreateResponse);
+	const contractCreateReceipt = await contractCreateResponse.wait();
+	console.log("contractCreateResponse.wait() mined receipt: ");
+    console.dir(contractCreateReceipt, {depth: null});
+
 	const abi = JSON.parse(readFileSync('examples/assets/abi/GLDToken_abi.json').toString());
 	// @ts-ignore
 	const contract = hethers.ContractFactory.getContract(contractCreateResponse.customData.contractId, abi, wallet);
-	const params = contract.interface.encodeFunctionData('approve', [
+	const approveParams = contract.interface.encodeFunctionData('approve', [
 		getAddressFromAccount(account.operator.accountId),
 		1000
 	]);
 	const approveResponse = await wallet.sendTransaction({
 		to: contract.address,
-		data: params,
+		data: approveParams,
 		gasLimit: 100000
 	});
-	console.log(approveResponse);
+	console.log("approveResponse", approveResponse);
+	const approveReceipt = await approveResponse.wait();
+	console.log("approveResponse.wait() mined receipt: ");
+    console.dir(approveReceipt, {depth: null});
+
+	const mintParams = contract.interface.encodeFunctionData('mint', [ 1000000000 ]);
+	const mintResponse = await wallet.sendTransaction({
+		to: contract.address,
+		data: mintParams,
+		gasLimit: 100000
+	});
+	console.log("mintResponse", mintResponse);
+	const mintReceipt = await mintResponse.wait();
+	console.log("mintResponse.wait() mined receipt: ");
+    console.dir(mintReceipt, {depth: null});
 })();
