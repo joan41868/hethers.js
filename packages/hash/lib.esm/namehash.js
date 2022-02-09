2b18a137b2b317e17f23eea50a34216e2a61a386
Yoan Sredkov <yoansredkov@gmail.com>
2022-02-09 23:58:58 +0200

    chore: build files
    Signed-off-by: Yoan Sredkov <yoansredkov@gmail.com>

--- packages/hash/lib.esm/namehash.js
+++ packages/hash/lib.esm/namehash.js
@@ -1,17 +1,20 @@
-import { concat, hexlify } from "@ethersproject/bytes";
-import { nameprep, toUtf8Bytes } from "@ethersproject/strings";
-import { keccak256 } from "@ethersproject/keccak256";
-import { Logger } from "@ethersproject/logger";
-import { version } from "./_version";
-const logger = new Logger(version);
-const Zeros = new Uint8Array(32);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.namehash = exports.isValidName = void 0;
+var bytes_1 = require("@ethersproject/bytes");
+var strings_1 = require("@ethersproject/strings");
+var keccak256_1 = require("@ethersproject/keccak256");
+var logger_1 = require("@ethersproject/logger");
+var _version_1 = require("./_version");
+var logger = new logger_1.Logger(_version_1.version);
+var Zeros = new Uint8Array(32);
 Zeros.fill(0);
-const Partition = new RegExp("^((.*)\\.)?([^.]+)$");
-export function isValidName(name) {
+var Partition = new RegExp("^((.*)\\.)?([^.]+)$");
+function isValidName(name) {
     try {
-        const comps = name.split(".");
-        for (let i = 0; i < comps.length; i++) {
-            if (nameprep(comps[i]).length === 0) {
+        var comps = name.split(".");
+        for (var i = 0; i < comps.length; i++) {
+            if ((0, strings_1.nameprep)(comps[i]).length === 0) {
                 throw new Error("empty");
             }
         }
@@ -25,17 +28,18 @@ export function namehash(name) {
     if (typeof (name) !== "string") {
         logger.throwArgumentError("invalid ENS name; not a string", "name", name);
     }
-    let current = name;
-    let result = Zeros;
+    var current = name;
+    var result = Zeros;
     while (current.length) {
-        const partition = current.match(Partition);
+        var partition = current.match(Partition);
         if (partition == null || partition[2] === "") {
             logger.throwArgumentError("invalid ENS address; missing component", "name", name);
         }
-        const label = toUtf8Bytes(nameprep(partition[3]));
-        result = keccak256(concat([result, keccak256(label)]));
+        var label = (0, strings_1.toUtf8Bytes)((0, strings_1.nameprep)(partition[3]));
+        result = (0, keccak256_1.keccak256)((0, bytes_1.concat)([result, (0, keccak256_1.keccak256)(label)]));
         current = partition[2] || "";
     }
-    return hexlify(result);
+    return (0, bytes_1.hexlify)(result);
 }
+exports.namehash = namehash;
 //# sourceMappingURL=namehash.js.map
